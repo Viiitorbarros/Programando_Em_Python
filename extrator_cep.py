552df2f8423d2a19b218027a5374e63c23e3bd30
Vitor Barros <vitorbarros.pgr@gmail.com>
2023-01-25 16:37:34 -0300

    Create extrator_cep.py

---
+++ extrator_cep.py
@@ -0,0 +1,16 @@
+endereço = 'Rua das Flores 72, apartamento 1002,Laranjeiras, Rio de Janeiro, RJ, 23440-120 '
+
+import re #Regular Expression -- RegEX
+
+#5 Digitos + hífen (opcional) + 3 digitos
+
+padrao = re.compile('[0-9]{5}[-]{0,1}[0-9]{3}')
+
+busca = padrao.search(endereço) # macth
+
+if busca :
+    cep = busca.group()
+    print(cep)
+else:
+    print('Não contém CEP')
+
